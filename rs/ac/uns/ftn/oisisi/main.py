9cc4545db9b9b7bcf3497d3ee61a88d429e9c2a0
marija.vucetic <marijavucetic@uns.ac.rs>
2020-02-21 13:04:16 +0100

    #main dodavanje main-a

---
+++ rs/ac/uns/ftn/oisisi/main.py
@@ -0,0 +1,40 @@
+
+#import tasks
+#import repositoryUtils as ru
+
+
+def pickOption(option):     #u zavisnosti koju opciju izaberemo ,odredjena funkcija se izvrsava(varijabla ima pokazivac na funkciju)
+    switcher = {
+        0: tasks.exit,
+        1: tasks.directorySelection,
+        4: tasks.wordSearch,
+      #  #  5: tasks.printContent,
+    }
+    func = switcher.get(option, lambda: "pogresna opcija")
+    func()
+    # Ako smo odabrali izlaz, nasilno izadji i ne pitaj nista
+    if option != 0:
+        quitOpt = input(" .. press any key to continue ... ")
+
+
+n = -1  # predefinisana peginacija
+
+
+def main():
+    option = -1
+    while option != 0:
+        try:
+            #ru.cls()  # brisanje ekrana
+            print("[1] Odabir direktorijuma")
+            print("[2] Unesi n za paginaciju") #kolio hocemo rezultata
+            print("[3] Pretraga reci ")
+            print("[4] Ispisi sadrzaj")
+            print("[0] Prekid")
+
+            option = int(input())
+            pickOption(option)
+        except ValueError:
+            print("Unesite neku od ponudjenih opcija:")
+
+
+main()
