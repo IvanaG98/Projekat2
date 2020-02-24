5a29a00b60004d47c7d197299e8de4376d288464
Ivana Gavric <ivanagavric1998@uns.ac.rs>
2020-02-24 01:33:13 +0100

    #prikaz rezultata

---
+++ rs/ac/uns/ftn/oisisi/ispis.py
@@ -0,0 +1,20 @@
+
+class Ispis(object):
+
+    def prikazRezultata(self,recnik,lista,lista1):
+        print("LEGENDA:\n")
+        print("Rang - R\nLink stranice - L\nBroj linkova ka njoj - BRL\nBroj rijeci na njoj - BRR\nBroj rijeci na stranicama koje ukazuju na nju - BRLR\nBroj rijeci iz pretrage koje se pojavljuju na toj stranici - BRPR\n")
+        print("R                    L               BRL     BRR       BRLR       BRPR\n")
+        lista = list(recnik.keys())
+        n = len(lista)
+        for i in range(n):
+            for j in range(0, n - i - 1):
+                if lista[j] < lista[j + 1]:
+                    lista[j], lista[j + 1] = lista[j + 1], lista[j]
+        for i in range(n):
+            key = lista[i]
+            pomocna = recnik[key]
+            print(key, pomocna)
+
+
+
